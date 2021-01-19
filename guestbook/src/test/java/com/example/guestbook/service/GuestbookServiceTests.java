8e1eed99d463c3630ca5e0668b275981fb2692ea
hyeyeon-choi <chlgpdus921@naver.com>
2021-01-20 00:55:59 +0900

    목록 페이지 구현 - DTO Setting
    close #5

--- guestbook/src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/com/example/guestbook/service/GuestbookServiceTests.java
@@ -1,22 +1,37 @@
 package com.example.guestbook.service;
 
 import com.example.guestbook.dto.GuestbookDTO;
+import com.example.guestbook.dto.PageRequestDTO;
+import com.example.guestbook.dto.PageResultDTO;
+import com.example.guestbook.entity.Guestbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class GuestbookServiceTests {
-@Autowired
+    @Autowired
     private GuestbookService service;
 
-@Test
-    public void testRegister(){
+    @Test
+    public void testRegister() {
         GuestbookDTO guestbookDTO = GuestbookDTO.builder()
                 .title("Sample title")
                 .content("Sample content")
                 .writer("user0").build();
 
-    System.out.println(service.register(guestbookDTO));
-}
+        System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+    }
 }
